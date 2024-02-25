ab32a4454fb381f77343603364826ec0a9801827
umemak <umematsu@gmail.com>
2024-02-25 12:19:05 +0900

    add db save

--- lambda/extract_text/extract_text.py
+++ lambda/extract_text/extract_text.py
@@ -4,14 +4,16 @@ import boto3
 import io
 import os
 from pdfminer.high_level import extract_text
+import pymysql
+
 
 def handler(event, context):
     # S3からPDFファイルを取得
-    s3 = boto3.client('s3')
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
+    s3 = boto3.client("s3")
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
     file_obj = s3.get_object(Bucket=bucket, Key=key)
-    file_content = file_obj['Body'].read()
+    file_content = file_obj["Body"].read()
     file_like_object = io.BytesIO(file_content)
 
     # PDFファイルのテキストを抽出
@@ -19,4 +21,29 @@ def handler(event, context):
 
     # 抽出したテキストをS3に保存
     base = os.path.splitext(os.path.basename(key))[0]
-    s3.put_object(Body=text,Bucket=bucket,Key=base+'.txt')
+    s3.put_object(Body=text, Bucket=bucket, Key=base + ".txt")
+
+    # 抽出したテキストをMySQLに保存
+    # RDSのMySQLインスタンスに接続
+    rds_client = boto3.client("rds")
+    db_instances = rds_client.describe_db_instances()
+    db_instance = db_instances["DBInstances"][0]
+    endpoint = db_instance["Endpoint"]["Address"]
+
+    # MySQLデータベースに接続
+    connection = pymysql.connect(
+        host=endpoint,
+        user=os.environ["DB_USER"],
+        passwd=os.environ["DB_PASSWORD"],
+        db=os.environ["DB_NAME"],
+    )
+    cursor = connection.cursor()
+
+    # テキストデータを保存するSQL文を実行
+    insert_query = "INSERT INTO text_table (file_name, text_data) VALUES (%s, %s)"
+    cursor.execute(insert_query, (base, text))
+    connection.commit()
+
+    # データベース接続を閉じる
+    cursor.close()
+    connection.close()
